39b5ca5d1b89862fe86e513233105a72fe63037d
mamadou adiouma <adiou.xyz@gmail.com>
2024-12-27 23:43:31 +0000

    Ajout du fichier de la somme de nombres aleatoires v1.

--- somme_deux_n.py
+++ somme_deux_n.py
@@ -3,8 +3,8 @@ def somme_de_deux_nombres():
     score = 0
     response = True
     while response:
-        aa = random.randint(0, 9)
-        bb = random.randint(0, 9)
+        aa = random.randint(0, 15)
+        bb = random.randint(0, 15)
         
         s = int(input("Somme des deux nombres ({aa} et {bb}) générés aléatoirement ? ".format(aa = aa, bb = bb)))
         vraie_somme = aa + bb
@@ -14,10 +14,8 @@ def somme_de_deux_nombres():
         else:
             response = False
             print("")
-            print("GAME OVER !")
-            print("FIN DU PROGRAMME")
+            print("RESULTAT :")
             break
-    print("Votre score final est - : ", score)
-            
-
+    print("Votre score final est  : ", score)
+    print("FIN DU PROGRAMME")
 somme_de_deux_nombres()
